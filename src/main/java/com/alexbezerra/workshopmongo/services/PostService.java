7aae86f2d8c66e89fab683a026c54f16d1ef8b56
Alex Bezerra <alexgbezerra@hotmail.com>
2023-04-03 17:33:35 -0300

    Consulta com varios criterios

--- src/main/java/com/alexbezerra/workshopmongo/services/PostService.java
+++ src/main/java/com/alexbezerra/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.alexbezerra.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,7 +16,7 @@ public class PostService {
 
 	@Autowired
 	private PostRepository repo;
-
+	
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
@@ -23,5 +24,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
