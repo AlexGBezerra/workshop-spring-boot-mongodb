9c9b6157287a5e645b2341bb516477c064138038
Alex Bezerra <alexgbezerra@hotmail.com>
2023-04-03 16:31:52 -0300

    Consulta simples com query methods

--- src/main/java/com/alexbezerra/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexbezerra/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.alexbezerra.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.alexbezerra.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
+
